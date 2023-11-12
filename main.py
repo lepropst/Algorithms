253a5c821c279ca5b98944f6f3f9853526b8037a
Eli R <elir@Elis-MacBook-Pro.local>
2023-11-12 16:20:55 -0500

    chore: finalized review 2

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import sys
 from heaps.max_heap import create_max_heap
 from heaps.min_heap import create_min_heap
+from logic.task_scheduling import schedule_tasks, task
+from sorts.bin_sort import binSort, binSortLexical
+from sorts.bubble_sort import BubbleSort
+
+# from sorts.bucket_sort import bucketSort
 from sorts.radix_sort import radixSort
 
 from trees.avl_tree import AVLTree
@@ -20,19 +25,36 @@ if __name__ == "__main__":
         f = json.loads(s)
     if f.get("data"):
         li = f.get("data")
-        # huffman_tree = construct_huffman_coding_tree(f.get("data"))
-        # huffman_tree.print_tree("huffman tree", dir="huffman")
-        # root = construct_binary_tree(li)
-        # root.print_tree("Saample bst")
-
+        lex_li = f.get("data_lexical")
+        task_li = f.get("data_tasks")
+        ###### TREES
         # root = AVLTree(li)
         # for x in li[1:]:
         #     root.insert(x)
         # root.print_tree("sample avl_tree")
 
+        # root = construct_binary_tree(li)
+        # root.print_tree("Saample bst")
+
+        ####### SORTS
         # radixSort(li)
+        # print(bucketSort(li))
+        # print(binSort(li))
+        # print(binSortLexical(lex_li))
+        print(BubbleSort(li))
+        ####### HEAPS
         # max_heap = create_max_heap(li)
         # print(max_heap)
         # min_heap = create_min_heap(li)
-        # print(min_heap)
+        # min_heap.remove()
+        # # print(min_heap)
+
+        ######## WEEK 4
+        # huffman_tree = construct_huffman_coding_tree(f.get("data"))
+        # huffman_tree.print_tree("huffman tree", dir="huffman")
+
+        # Task scheduling
+        # tasks = [task(x.get("start")[:-1], x.get("end")[:-1]) for x in task_li]
+        # schedule_tasks(tasks)
+        # print(tasks)
         exit()
